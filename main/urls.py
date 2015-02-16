4a41b3bc15e3cb2049c490b78056c23b7b13c733
Ian Humphrey <irh9@nau.edu>
2015-02-15 17:00:17 -0700

    added routing for home template

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'main.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'main.views.home.index', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
