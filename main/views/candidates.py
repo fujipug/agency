31e0f606fd7cf44b04a1281d906f644d8d2c38e1
itj3 <itj3@nau.edu>
2015-02-18 23:13:54 -0700

    updated candidates.py to show individual pages
    No longer shows the template

--- main/views/candidates.py
+++ main/views/candidates.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
-
-def index(request):
-    return render(request, 'main/candidates.html', {})
+def index(request, candidate_id):
+    candidate = Candidate.objects.get(id=candidate_id)
+    template = get_template('main/candidates_base.html')
+    html = template.render(Context({'c' : candidate}))
+    return HttpResponse(html)
