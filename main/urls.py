7664e63d639953e71eedc82d3296791eab3b9820
itj3 <itj3@nau.edu>
2015-02-18 23:23:47 -0700

    everything should be back to normal now...

--- main/urls.py
+++ main/urls.py
@@ -8,12 +8,10 @@ urlpatterns = patterns('',
 
     url(r'^$', 'main.views.home.index', name='home'),
     url(r'^about/', 'main.views.home.about', name='about'),
-    url(r'^candidates/$', 'main.views.candidates.index', name='candidates'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^free_stuff_form/', 'main.views.views.free_stuff_form', name='free_stuff_form'),
-    url(r'^candidates/(?P<candidate_id>\d+)/$','main.views.views.Candidates'),
     url(r'^confirmation/', 'main.views.confirmation.index', name='Confirmation'),
     url(r'^democrat_list/', 'main.views.democrat_list.democrat_list', name='Democrats'),
     url(r'^republican_list/', 'main.views.republican_list.republican_list', name='Republicans'),
-    url(r'^aboutcandidates/(?P<candidate_id>\d+)/$','main.views.views.Candidates'),
+    url(r'^candidates/(?P<candidate_id>\d+)/$', 'main.views.candidates.index', name='candidates'),
 )
--- main/views/candidates.py
+++ main/views/candidates.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from main.models import Candidate
+from django.template import Context
 
 
-def index(request):
-    return render(request, 'main/candidates.html', {})
+def index(request, candidate_id):
+    candidate = Candidate.objects.get(id=candidate_id)
+    context = {'c': candidate}
+    return render(request, 'main/candidates_base.html', context)
+   # template = get_template('main/candidates_base.html')
+   # html = template.render(Context({'c' : candidate}))
+   # return HttpResponse(html)
