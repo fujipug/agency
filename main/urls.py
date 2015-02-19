73662aecc9ce1cf6b94ec4b65598f3c7e69abe40
itj3 <itj3@nau.edu>
2015-02-18 17:01:28 -0700

    cleaning up urls.py

--- main/urls.py
+++ main/urls.py
@@ -7,9 +7,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', 'main.views.home.index', name='home'),
-    #url(r'^candidates/', 'main.views.candidates.index', name='candidates'),
+    url(r'^candidates/', 'main.views.candidates.index', name='candidates'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^post_form_upload', 'main.views.views.post_form_upload', name='post_form_upload'),
-    url(r'^Candidate_1', 'main.views.views.Candidate_1'),
     url(r'^candidates/(?P<candidate_id>\d+)/$','main.views.views.Candidates'),
 )
