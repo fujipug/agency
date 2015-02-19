d9aec93b32a85852be21b9de06f72f775adddcc6
itj3 <itj3@nau.edu>
2015-02-18 23:18:59 -0700

    Revert "updated candidates.py to show individual pages"
    This reverts commit 31e0f606fd7cf44b04a1281d906f644d8d2c38e1.

--- main/views/candidates.py
+++ main/views/candidates.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-def index(request, candidate_id):
-    candidate = Candidate.objects.get(id=candidate_id)
-    template = get_template('main/candidates_base.html')
-    html = template.render(Context({'c' : candidate}))
-    return HttpResponse(html)
+
+def index(request):
+    return render(request, 'main/candidates.html', {})
