59c40f3f12e649282dde9183c52d57a023263f2a
Lane <lf237@nau.edu>
2015-02-18 21:46:33 -0700

    Democrats list working in the navbar

--- main/urls.py
+++ main/urls.py
@@ -9,14 +9,10 @@ urlpatterns = patterns('',
     url(r'^$', 'main.views.home.index', name='home'),
     url(r'^candidates/', 'main.views.candidates.index', name='candidates'),
     url(r'^admin/', include(admin.site.urls)),
-<<<<<<< HEAD
     url(r'^free_stuff_form/', 'main.views.views.free_stuff_form', name='free_stuff_form'),
     url(r'^candidates/(?P<candidate_id>\d+)/$','main.views.views.Candidates'),
     url(r'^confirmation/', 'main.views.confirmation.index', name='Confirmation'),
     url(r'^democrat_list/', 'main.views.democrat_list.democrat_list', name='Confirmation'),
 
-=======
-    url(r'^post_form_upload', 'main.views.views.post_form_upload', name='post_form_upload'),
     url(r'^aboutcandidates/(?P<candidate_id>\d+)/$','main.views.views.Candidates'),
->>>>>>> d6753f66465fd1eb6a38d35472bafc097f4e0b4e
 )
